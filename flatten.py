20625a4d418708c3a8aee15ff766c11b1dcdf409
Daryubel - Ryzen DSKTP <he19991017@hotmail.com>
2022-05-21 15:02:55 +0800

    Updated code

---
+++ flatten.py
@@ -0,0 +1,67 @@
+
+import numpy as np
+from sympy import Symbol, cos, integrate, solve
+import smallest
+
+e = 2.71828
+pi = 3.14159
+
+class flattenModelFunctionClass:
+
+    x = Symbol('x')
+    c = Symbol('c')
+    f = 1
+    def h(i,x):
+        return (1-e**(-i*x)) / i
+    hList = []
+    hVal = []
+    hList.append(x)
+    hVal.append(1)
+    for i in range(1,11):
+        hList.append(h(i,x))
+        hVal.append(h(i,1))
+    print(hVal)
+
+
+class flattenModelParamClass:
+    
+    # method for cartesian product of (gi, gj)
+    def cartesianProduct(i, j):
+        f = flattenModelFunctionClass.f
+        gl = flattenModelFunctionClass.hList[i]
+        gr = flattenModelFunctionClass.hList[j]
+        x = flattenModelFunctionClass.x
+
+        return integrate((gl/f) * (gr/f), (x, 0, 1))
+
+    # method for deciding constant c
+    def rhoVal(x, rho):
+        c = flattenModelFunctionClass.c
+        return eval(str(rho-0.5))
+
+    xVec = np.linspace(0,1,11)
+    y = np.zeros(11)
+    for i in range(0,11):
+        y[i] = 0.5 * flattenModelFunctionClass.hVal[i] - smallest.smallestModelParamClass.d[i]
+    
+    print(y)
+    
+    # H matrix
+    matrixH = np.zeros((11,11))
+    for j in range(0, 11):
+        for i in range(0, 11):
+            matrixH[i][j] = cartesianProduct(i, j)
+    
+    B = np.dot(np.linalg.inv(matrixH), y)
+
+    h_over_f = []
+    for i in range(0, 11):
+        h_over_f.append(flattenModelFunctionClass.hList[i] / (flattenModelFunctionClass.f**2))
+    x = flattenModelFunctionClass.x
+        
+    mMid = np.dot(B, h_over_f) + flattenModelFunctionClass.c
+    c = solve(rhoVal(1, mMid), flattenModelFunctionClass.c)[0]
+
+    ############################################
+    # return the symbolized rho expression
+    rho = str(np.dot(B, h_over_f) + c)
---
+++ smallest.py
@@ -0,0 +1,55 @@
+
+import numpy as np
+from sympy import Symbol, cos, integrate
+
+e = 2.71828
+pi = 3.14159
+
+class smallestModelFunctionClass:
+
+    x = Symbol('x')
+    f = 1
+    def m(x):
+        return 1 - 0.5 * cos(2*pi*x)
+    def g(i, x):
+        return e**(-(i-1)*x)
+    gList = []
+    for i in range(1,11+1):
+        gList.append(g(i, x))
+
+
+class smallestModelParamClass:
+    
+    # method for cartesian product of (gi, gj)
+    def cartesianProduct(i, j):
+        f = smallestModelFunctionClass.f
+        gl = smallestModelFunctionClass.gList[i]
+        gr = smallestModelFunctionClass.gList[j]
+        x = smallestModelFunctionClass.x
+
+        return integrate((gl/f) * (gr/f), (x, 0, 1))
+
+    xVec = np.linspace(0,1,11)
+    d = np.zeros(11)
+    m = np.zeros(11)
+    for i in range(0,11):
+        m[i] = smallestModelFunctionClass.m(xVec[i])
+        d[i] = integrate(smallestModelFunctionClass.m(smallestModelFunctionClass.x) * smallestModelFunctionClass.g(i+1, smallestModelFunctionClass.x), 
+                        (smallestModelFunctionClass.x, 0, 1))
+    
+    # G matrix
+    matrixG = np.zeros((11,11))
+    for j in range(0, 11):
+        for i in range(0, 11):
+            matrixG[i][j] = cartesianProduct(i, j)
+    
+    a = np.dot(np.linalg.inv(matrixG), d)
+
+    g_over_f = []
+    for i in range(0, 11):
+        g_over_f.append(smallestModelFunctionClass.gList[i] / (smallestModelFunctionClass.f**2))
+    x = smallestModelFunctionClass.x
+        
+
+    # return the symbolized rho expression
+    m = str(np.dot(a, g_over_f))
